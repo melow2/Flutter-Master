1b6894247102de4dc260806995b7962ac080d959
bread <kimyeonsik@gmail.com>
2021-12-07 16:23:36 +0900

    quiz_app 제거, 일부 빌드 오류 수정

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:quiz_flutter/quiz_app/quiz_app_stateless.dart';
+import 'package:quiz_flutter/order_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
