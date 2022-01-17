0c347d43fdecb802c0925d0fe106a3325889f35c
amy <melow2@naver.com>
2022-01-17 09:19:53 +0900

    [개발] HomeScreen - 액션바 개발.

--- test/FurnitureApp/FurnitureApp.dart
+++ test/FurnitureApp/FurnitureApp.dart
@@ -30,6 +30,7 @@ class FurnitureApp extends StatelessWidget {
           ),
           visualDensity: VisualDensity.adaptivePlatformDensity
       ),
+      home: HomeScreen(),
     );
   }
 }
--- test/FurnitureApp/home/HomeScreen.dart
+++ test/FurnitureApp/home/HomeScreen.dart
@@ -1,17 +1,44 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class HomeScreen extends StatefulWidget {
+import '../data/Constants.dart';
+import '../data/SizeConfig.dart';
+
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    SizeConfig.init(context);
+    return Scaffold(
+      body: Container(),
+      appBar: buildAppBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      leading: IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/menu.svg",
+            height: SizeConfig.defaultSize! * 2.0,
+          ),
+          onPressed: () {}),
+      actions: [
+        IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              "assets/icons/scan.svg",
+              height: SizeConfig.defaultSize! * 2.4,
+            )),
+        const Center(
+          child: Text(
+            "Scan",
+            style: TextStyle(color: kTextColor, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(width: SizeConfig.defaultSize)
+      ],
+    );
   }
 }
