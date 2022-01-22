e1c486b9dbc2083b8a7d1643b7bb77df25981293
amy <melow2@naver.com>
2022-01-22 15:17:45 +0900

    [개발] DetailScreen - #6 좌측 텍스트 뷰 완성.

--- test/FurnitureApp/detail/details_body.dart
+++ test/FurnitureApp/detail/details_body.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../data/Constants.dart';
 import '../data/SizeConfig.dart';
 import '../models/Product.dart';
+import 'details_product_info.dart';
 
 class DetailBody extends StatelessWidget {
   final Product product;
@@ -12,50 +14,7 @@ class DetailBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-        child: detail_product_info(
-      product: product,
-    ));
-  }
-}
-
-class detail_product_info extends StatelessWidget {
-  const detail_product_info({
-    Key? key,
-    required this.product,
-  }) : super(key: key);
-
-  final Product product;
-
-  @override
-  Widget build(BuildContext context) {
-    double defaultSize = SizeConfig.defaultSize!;
-    TextStyle lightTextStyle = TextStyle(color: kTextColor.withOpacity(0.6));
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: defaultSize * 2),
-      height: defaultSize * 37.5,
-      width: defaultSize * 15,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(product.category.toUpperCase(), style: lightTextStyle),
-          SizedBox(height: defaultSize),
-          Text(
-            product.title,
-            style:
-                TextStyle(fontSize: defaultSize * 2.4, fontWeight: FontWeight.bold, letterSpacing: -0.8, height: 1.4),
-          ),
-          SizedBox(height: defaultSize * 2),
-          Text(
-            "Form",
-            style: lightTextStyle,
-          ),
-          Text(
-            "\$${product.price}",
-            style: TextStyle(fontSize: defaultSize, fontWeight: FontWeight.bold, height: 1.6),
-          )
-        ],
-      ),
+        child: DetailProductInfo(product: product,)
     );
   }
 }
---
+++ test/FurnitureApp/detail/details_product_info.dart
@@ -0,0 +1,73 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../data/Constants.dart';
+import '../data/SizeConfig.dart';
+import '../models/Product.dart';
+
+class DetailProductInfo extends StatelessWidget {
+  const DetailProductInfo({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    double defaultSize = SizeConfig.defaultSize!;
+    TextStyle lightTextStyle = TextStyle(color: kTextColor.withOpacity(0.6));
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: defaultSize * 2),
+      height: defaultSize * 37.5,
+      width: defaultSize * 16,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(product.category.toUpperCase(), style: lightTextStyle),
+          SizedBox(height: defaultSize),
+          Text(
+            product.title,
+            style:
+            TextStyle(fontSize: defaultSize * 2.4, fontWeight: FontWeight.bold, letterSpacing: -0.8, height: 1.4),
+          ),
+          SizedBox(height: defaultSize * 2),
+          Text(
+            "Form",
+            style: lightTextStyle,
+          ),
+          Text(
+            "\$${product.price}",
+            style: TextStyle(fontSize: defaultSize, fontWeight: FontWeight.bold, height: 1.6),
+          ),
+          SizedBox(
+            height: defaultSize * 2,
+          ),
+          Text(
+            "Available Colors",
+            style: lightTextStyle,
+          ),
+          Row(
+            children: [
+              buildColorBox(defaultSize,color: Color(0xFF7BA275), isActive: true),
+              buildColorBox(defaultSize,color: Color(0xFFD7D7D7), isActive: false),
+              buildColorBox(defaultSize,color: kTextColor, isActive: false),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Container buildColorBox(double defaultSize, {required Color color, required bool isActive}) {
+    return Container(
+        margin: EdgeInsets.only(top: defaultSize, right: defaultSize),
+        padding: EdgeInsets.all(5),
+        height: defaultSize * 2.4,
+        width: defaultSize * 2.4,
+        decoration: BoxDecoration(color: color, borderRadius: BorderRadius.circular(6)),
+        child: isActive ? SvgPicture.asset("assets/icons/check.svg") : SizedBox());
+  }
+}
