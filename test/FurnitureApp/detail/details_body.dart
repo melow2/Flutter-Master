8e99603c2944f37428b5f0e26c30fb297dd13f68
amy <melow2@naver.com>
2022-01-22 15:05:51 +0900

    [개발] DetailScreen - #5 좌측 텍스트 뷰 그리기.

--- test/FurnitureApp/detail/details_body.dart
+++ test/FurnitureApp/detail/details_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../data/Constants.dart';
 import '../data/SizeConfig.dart';
 import '../models/Product.dart';
 
@@ -10,13 +11,51 @@ class DetailBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double defaultSize = SizeConfig.defaultSize!;
     return SingleChildScrollView(
-      child: Container(
-        height: defaultSize * 37.5,
-        width: defaultSize * 15,
-        color: Colors.blueGrey,
-      )
+        child: detail_product_info(
+      product: product,
+    ));
+  }
+}
+
+class detail_product_info extends StatelessWidget {
+  const detail_product_info({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    double defaultSize = SizeConfig.defaultSize!;
+    TextStyle lightTextStyle = TextStyle(color: kTextColor.withOpacity(0.6));
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: defaultSize * 2),
+      height: defaultSize * 37.5,
+      width: defaultSize * 15,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(product.category.toUpperCase(), style: lightTextStyle),
+          SizedBox(height: defaultSize),
+          Text(
+            product.title,
+            style:
+                TextStyle(fontSize: defaultSize * 2.4, fontWeight: FontWeight.bold, letterSpacing: -0.8, height: 1.4),
+          ),
+          SizedBox(height: defaultSize * 2),
+          Text(
+            "Form",
+            style: lightTextStyle,
+          ),
+          Text(
+            "\$${product.price}",
+            style: TextStyle(fontSize: defaultSize, fontWeight: FontWeight.bold, height: 1.6),
+          )
+        ],
+      ),
     );
   }
 }
