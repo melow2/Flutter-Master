a15622d3cbd8c359b7a3ef474a197560e75a5b2e
amy <melow2@naver.com>
2022-01-22 15:53:20 +0900

    [개발] DetailScreen - #7 하단 description view.

--- test/FurnitureApp/detail/details_body.dart
+++ test/FurnitureApp/detail/details_body.dart
@@ -13,8 +13,46 @@ class DetailBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double defaultSize = SizeConfig.defaultSize!;
     return SingleChildScrollView(
-        child: DetailProductInfo(product: product,)
-    );
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        DetailProductInfo(product: product),
+        Container(
+          constraints: BoxConstraints(
+              minHeight: defaultSize * 44
+          ),
+          padding: EdgeInsets.only(
+            top: defaultSize * 9,
+            left: defaultSize * 2,
+            right:  defaultSize *2
+          ),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(defaultSize * 1.2), topRight: Radius.circular(defaultSize * 1.2))),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                product.subTitle,
+                style: TextStyle(
+                    fontSize: defaultSize * 1.8,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: defaultSize * 3,),
+              Text(
+                product.description,
+                style: TextStyle(
+                    color: kTextColor.withOpacity(0.7),
+                  height: 1.5
+                ),)
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
