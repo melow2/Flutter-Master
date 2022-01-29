d88e083541019643c93cc06a22e655adc89ef797
amy <melow2@naver.com>
2022-01-29 12:46:35 +0900

    [개발] HomeBody - 프로덕트 아이템.

--- test/FurnitureApp/home/component/body.dart
+++ test/FurnitureApp/home/component/body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../../data/Constants.dart';
 import '../../data/SizeConfig.dart';
+import '../../models/Categories.dart';
 import '../../widget/title_text.dart';
 
 class Body extends StatelessWidget {
@@ -11,15 +13,79 @@ class Body extends StatelessWidget {
     double defaultSize = SizeConfig.defaultSize!;
     return SingleChildScrollView(
       child: Column(
-        children:  [
+        children: [
           Padding(
             padding: EdgeInsets.all(defaultSize * 2),
             child: const TitleText(
               title: 'Browse by Categories',
             ),
+          ),
+          Container(
+            width: defaultSize * 20.5,
+            height: defaultSize * 23.5,
+            child: Stack(alignment: Alignment.bottomCenter, children: [
+              ClipPath(
+                clipper: CategoryCustomShape(),
+                child: AspectRatio(
+                  aspectRatio: 1.2 / 1,
+                  child: Container(
+                    padding: EdgeInsets.all(defaultSize * 2),
+                    color: kSecondaryColor,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TitleText(
+                          title: category.title,
+                        ),
+                        SizedBox(height: defaultSize),
+                        Text(
+                          "${category.numOfProducts} + Products",
+                          style: TextStyle(color: kTextColor.withOpacity(0.6)),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                  top: 0,
+                  right: 0,
+                  left: 0,
+                  child: AspectRatio(
+                    aspectRatio: 1.15,
+                    child: FadeInImage.assetNetwork(placeholder: "assets/spinner.gif", image: category.image),
+                  ))
+            ]),
           )
         ],
       ),
     );
+  }
+}
+
+class CategoryCustomShape extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    double height = size.height;
+    double width = size.width;
+    double cornerSize = 30;
+
+    path.lineTo(0, height - cornerSize);
+    path.quadraticBezierTo(0, height, cornerSize, height);
+    path.lineTo(width - cornerSize, height);
+    path.quadraticBezierTo(width, height, width, height - cornerSize);
+    path.lineTo(width, cornerSize);
+    path.quadraticBezierTo(width, 0, width - cornerSize, 0);
+    path.lineTo(cornerSize, cornerSize * 0.75);
+    path.quadraticBezierTo(0, cornerSize, 0, cornerSize * 2);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
