5e67d88d0d2c67b7434bda5e7b78e22dcd22c86b
amy <melow2@naver.com>
2022-01-29 11:24:30 +0900

    [개발] AppBar - 개발.

--- test/FurnitureApp/app.dart
+++ test/FurnitureApp/app.dart
@@ -13,17 +13,22 @@ void main(){
 class FurnitureApp extends StatelessWidget {
   const FurnitureApp({Key? key}) : super(key: key);
 
-  /**
-   * MaterialApp 그리기.
-   *
-   * 1) 타이틀
-   * 2) 디버그시 우측 상단 밴드 표시 하지않음.
-   * 3) Theme - 텍스트, 앱바, scaffold background.
-   * 4) visualDenscity
-   * 5) 홈스크린.
-   */
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Furniture App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: GoogleFonts.dmSansTextTheme().apply(displayColor: kTextColor),
+        appBarTheme: AppBarTheme(
+          color: Colors.transparent,
+          elevation: 0,
+          brightness: Brightness.light
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity
+      ),
+      home: HomeScreen(),
+    );
   }
 }
--- test/FurnitureApp/home/home_screen.dart
+++ test/FurnitureApp/home/home_screen.dart
@@ -1,12 +1,38 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../data/Constants.dart';
+import '../data/SizeConfig.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    SizeConfig.init(context);
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: SvgPicture.asset("assets/icons/menu.svg", height: SizeConfig.defaultSize! * 2),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: SvgPicture.asset(
+              "assets/icons/scan.svg",
+              height: SizeConfig.defaultSize! * 2.4,
+            ),
+            onPressed: () {},
+          ),
+          Center(
+            child: Text(
+              "Scan",
+              style: TextStyle(color: kTextColor, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(width: SizeConfig.defaultSize,)
+        ],
+      ),
+    );
   }
 }
