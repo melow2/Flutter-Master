b8d5eaaed057f865ce082aa10962b133bf6d3393
amy <melow2@naver.com>
2022-01-30 11:02:14 +0900

    [개발] HomeBody - #2 앱바 추가.

--- test/FurnitureApp/detail/detail_screen.dart
+++ test/FurnitureApp/detail/detail_screen.dart
@@ -1,19 +1,39 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
+import '../data/Constants.dart';
+import '../data/SizeConfig.dart';
 import '../models/Product.dart';
 
-
 class DetailScreen extends StatelessWidget {
-
   final Product product;
 
-  const DetailScreen({Key? key, required this.product }) : super(key: key);
+  const DetailScreen({Key? key, required this.product}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueAccent,
+    SizeConfig.init(context);
+    return Scaffold(
+      backgroundColor: kSecondaryColor,
+      appBar: buildAppBar(context),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: SvgPicture.asset("assets/icons/back.svg"),
+      ),
+      actions: [
+        IconButton(
+          icon: SvgPicture.asset("assets/icons/bag.svg"),
+          onPressed: () {},
+        ),
+        SizedBox(width: SizeConfig.defaultSize,)
+      ],
     );
   }
 }
