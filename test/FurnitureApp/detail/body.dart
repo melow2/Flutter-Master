3917981100115fc1b439a76ead59d9ed8f77cdea
amy <melow2@naver.com>
2022-01-30 11:46:57 +0900

    [개발] HomeBody - 상세화면 #5 하단 프로덕트 내용.

--- test/FurnitureApp/detail/body.dart
+++ test/FurnitureApp/detail/body.dart
@@ -40,6 +40,22 @@ class Body extends StatelessWidget {
                   Text(
                     product.description,
                     style: TextStyle(color: kTextColor.withOpacity(0.7), height: 1.5),
+                  ),
+                  SizedBox(
+                    height: defaultSize * 3,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: FlatButton(
+                        padding: EdgeInsets.all(defaultSize * 1.5),
+                        color: kPrimaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30)
+                        ),
+                        onPressed: () {},
+                        child: Text("Add to Cart",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: defaultSize * 1.6, fontWeight: FontWeight.bold))),
                   )
                 ],
               ),
